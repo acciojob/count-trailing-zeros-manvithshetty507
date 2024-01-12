9e8edc0b9baf7c822f3d82efb2baeb637f719bd5
Manvith Kumar shetty <121666002+manvithshetty507@users.noreply.github.com>
2024-01-12 15:52:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 function trailingZeros(n) {
-  //your JS code here. If required.
-	if(n == 0) return 0;
-	function fact(n) {
-		if(n == 1) return 1
-		return n * fact(n-1);
-	}
+    if (n === 0) return 0;
 
-	let num = fact(n);
+    function factorial(num) {
+        return num === 1 ? 1 : num * factorial(num - 1);
+    }
 
-	let count = 0;
-	while(num % 10 == 0) {
-		count++;
-		num = num/10
-	}
-	return count
+    const num = factorial(n);
+
+    let count = 0;
+    while (num % 10 === 0) {
+        count++;
+        num /= 10;
+    }
+    return count;
 }
 
-//const input = prompt("Enter a number");
+const input = prompt("Enter a number");
 alert(trailingZeros(input));
